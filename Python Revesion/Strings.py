a6385471be46ba1ed7a44126b59ff59c1f437312
Pranav Mishra <62085897+PRANAV40@users.noreply.github.com>
2025-10-05 23:59:40 +0530

    Add files via upload

---
+++ Python Revesion/Strings.py
@@ -0,0 +1,19 @@
+words = "Python"
+print(words)
+print(words[0:3])
+print(words[::-1])
+
+s = "Hello"
+t = "World"
+print(s+"",t)
+print(len(words))
+
+for i in "banana":
+    print(i)
+
+strs = " Hello Worlds! "
+print(strs.strip())
+
+print(strs.replace("H","J"))
+
+print(strs.split(","))
