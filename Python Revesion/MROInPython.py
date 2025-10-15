24cbc13776398c72823bc7bd8306cb606463547b
Pranav Mishra <62085897+PRANAV40@users.noreply.github.com>
2025-10-15 23:30:19 +0530

    Add files via upload

---
+++ Python Revesion/MROInPython.py
@@ -0,0 +1,20 @@
+class A:
+    def show(self):
+        print("A")
+
+class B(A):
+    def show(self):
+        print("B")
+
+class C(A):
+    def show(self):
+        print("C")
+
+class D(B,C):
+    pass
+
+obj = D()
+obj.show()
+
+# MRO order: D → B → C → A → object
+print(D.mro())
